a0d8be29a9e5a708f8092e40a7a483a99a41e89d
Sofialv05 <sofalvsy@gmail.com>
2024-08-06 14:44:38 +0700

    feat: seeder

--- models/Activity.js
+++ models/Activity.js
@@ -1,94 +1,117 @@
-const { GraphQLError } = require("graphql")
-const { DB } = require(`../config/db`)
-const { ObjectId } = require("mongodb")
+const { GraphQLError } = require("graphql");
+const { DB } = require(`../config/db`);
+const { ObjectId } = require("mongodb");
 
 class Activity {
-    static async addActivityForSeller(title, types, imgurls, description, tags, location, sellerUserId) {
-        const postCollection = DB.collection("activities")
-        let reviews = []
-        let createdAt = new Date()
-        let updatedAt = new Date()
-        let customers = []
-        let sellerId = new ObjectId(sellerUserId)
+  static async addActivityForSeller(
+    title,
+    types,
+    imgurls,
+    description,
+    tags,
+    location,
+    sellerUserId
+  ) {
+    const postCollection = DB.collection("activities");
+    let reviews = [];
+    let createdAt = new Date();
+    let updatedAt = new Date();
+    let customers = [];
+    let sellerId = new ObjectId(sellerUserId);
 
-        let result = await postCollection.insertOne({ title, types, imgurls, description, tags, sellerId, createdAt, updatedAt, reviews, customers, location })
-        return await postCollection.findOne({ _id: result.insertedId });
-    }
+    let result = await postCollection.insertOne({
+      title,
+      types,
+      imgurls,
+      description,
+      tags,
+      sellerId,
+      createdAt,
+      updatedAt,
+      reviews,
+      customers,
+      location,
+    });
+    return await postCollection.findOne({ _id: result.insertedId });
+  }
 
-    static async findAll() {
-        let pipeline = [
-            {
-                $sort:
-                {
-                    createdAt: -1,
-                },
-            },
-        ]
-        const postCollection = DB.collection("activities")
-        let result = await postCollection.aggregate(pipeline).toArray()
-        console.log(result)
-        return result
-    }
+  static async findAll() {
+    let pipeline = [
+      {
+        $sort: {
+          createdAt: -1,
+        },
+      },
+    ];
+    const postCollection = DB.collection("activities");
+    let result = await postCollection.aggregate(pipeline).toArray();
+    console.log(result);
+    return result;
+  }
 
-    static async search(searchTerm){
-        const activityCollection = DB.collection("activities")
-        let results = await activityCollection.find({ 
-            title: { 
-                $regex: new RegExp(searchTerm, "i")
-            } 
-        }).toArray()
-        console.log(results)
-        return results
-    }
+  static async search(searchTerm) {
+    const activityCollection = DB.collection("activities");
+    let results = await activityCollection
+      .find({
+        title: {
+          $regex: new RegExp(searchTerm, "i"),
+        },
+      })
+      .toArray();
+    console.log(results);
+    return results;
+  }
 
-    static async findById(_id) {
-        const postCollection = DB.collection("activities")
+  static async findById(_id) {
+    const postCollection = DB.collection("activities");
 
-        let pipeline = [
-            {
-                $match:
-                {
-                    _id: new ObjectId(_id),
-                },
-            }, {
-                $limit: 1
-            },
-        ]
-        let result = await postCollection.aggregate(pipeline).toArray()
-        return result[0]
-    }
+    let pipeline = [
+      {
+        $match: {
+          _id: new ObjectId(_id),
+        },
+      },
+      {
+        $limit: 1,
+      },
+    ];
+    let result = await postCollection.aggregate(pipeline).toArray();
+    return result[0];
+  }
 
-    static async findBySellerId(sellerUserId){
-        const postCollection = DB.collection("activities")
-        let pipeline = [
-            {
-                $match:
-                {
-                    sellerId: new ObjectId(sellerUserId),
-                },
-            },
-        ]
-        let result = await postCollection.aggregate(pipeline).toArray()
-        return result
-    }
+  static async findBySellerId(sellerUserId) {
+    const postCollection = DB.collection("activities");
+    let pipeline = [
+      {
+        $match: {
+          sellerId: new ObjectId(sellerUserId),
+        },
+      },
+    ];
+    let result = await postCollection.aggregate(pipeline).toArray();
+    return result;
+  }
 
-    static async updateActivityForseller(activityId, title, types, imgurls, description, tags, sellerId){
-        const postCollection = DB.collection("activities")
-        let pipeline = [
-            {
-                $match:
-                {
-                    sellerId: new ObjectId(sellerUserId),
-                },
-            },
-        ]
-        let result = await postCollection.aggregate(pipeline).toArray()
-        return result
-    }
-
-    
-
-    
+  static async updateActivityForseller(
+    activityId,
+    title,
+    types,
+    imgurls,
+    description,
+    tags,
+    sellerId
+  ) {
+    const postCollection = DB.collection("activities");
+    let pipeline = [
+      {
+        $match: {
+          sellerId: new ObjectId(sellerUserId),
+        },
+      },
+    ];
+    let result = await postCollection.aggregate(pipeline).toArray();
+    return result;
+  }
 }
 
-module.exports = Activity
+module.exports = Activity;
