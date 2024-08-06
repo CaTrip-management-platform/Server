0347ff41732c7247a93e946b06c4c7492fedcd9c
Sofialv05 <sofalvsy@gmail.com>
2024-08-06 15:21:38 +0700

    feat: create trip

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri);
 
-const DB = client.db("CaTrip")
+const DB = client.db("CaTrip");
 
 module.exports = {
-    client,
-    DB
-}
-
-
+  client,
+  DB,
+};
