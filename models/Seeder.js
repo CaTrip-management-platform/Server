aa9a23289879af98acf71020d0d63e2eed31bafb
Sofialv05 <sofalvsy@gmail.com>
2024-08-07 15:59:46 +0700

    fix: seeders

--- models/Seeder.js
+++ models/Seeder.js
@@ -2,21 +2,21 @@ const { ObjectId } = require("mongodb");
 const { DB } = require("../config/db");
 
 const seeders = async () => {
-  // const datas = require("../db.json");
+  const datas = require("../db.json");
   const postCollection = DB.collection("activities");
 
   console.log(postCollection);
   // console.log(datas);
-  // for (let data of datas) {
-  //   data = {
-  //     ...data,
-  //     sellerId: new ObjectId(data.sellerId),
-  //     createdAt: new Date(),
-  //     updatedAt: new Date(),
-  //   };
+  for (let data of datas) {
+    data = {
+      ...data,
+      userId: new ObjectId(data.userId),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
 
-  //   await postCollection.insertOne(data);
-  // }
+    await postCollection.insertOne(data);
+  }
 };
 
 seeders();
